8f9885785029907b404a266d073144251af0cf09
Kwark <kwark@github.com>
2018-02-02 20:50:54 +0530

    Files added
    The training data and the source codes are added

---
+++ image.py
@@ -0,0 +1,95 @@
+#For testing use only images of 8 by 8 dimensions. 
+from PIL import Image
+import numpy as np
+
+import matplotlib.pyplot as plt
+from collections import Counter
+def threshold(imageArray):
+    balanceAr = []
+    newAr = imageArray
+    for eachRow in imageArray:
+        for eachPix in eachRow:
+            avgNum = reduce(lambda x, y: x + y, eachPix[:3]) / len(eachPix[:3])
+            balanceAr.append(avgNum)
+    balance = reduce(lambda x, y: x + y, balanceAr) / len(balanceAr)
+    for eachRow in newAr:
+        for eachPix in eachRow:
+            if reduce(lambda x, y: x + y, eachPix[:3]) / len(eachPix[:3]) > balance:
+                eachPix[0] = 255
+                eachPix[1] = 255
+                eachPix[2] = 255
+                eachPix[3] = 255
+            else:
+                eachPix[0] = 0
+                eachPix[1] = 0
+                eachPix[2] = 0
+                eachPix[3] = 255
+    return newAr
+
+def whatNumIsThis(filePath):
+    
+    matchedAr = []
+    loadExamps = open('numArEx.txt','r').read()
+    loadExamps = loadExamps.split('\n')
+    
+    i = Image.open(filePath)
+    iar = np.array(i)
+#The next 5 lines of code only if alpha channel is turned off
+    z = np.ones((8,1),dtype = np.uint8)
+    z = z * 255
+    niar = np.zeros((8,8,4),dtype = np.uint8)
+    for i in range(0,8):
+        niar[i] = np.append(iar[i], z,axis = 1)
+    tiar = threshold(niar)#Use iar if alpha channel is turned on
+    iarl = tiar.tolist()
+    
+    inQuestion = str(iarl)
+    
+    for eachExample in loadExamps:
+        try:
+            splitEx = eachExample.split('::')
+            currentNum = splitEx[0]
+            currentAr = splitEx[1]
+            
+            eachPixEx = currentAr.split('],')
+            eachPixInQ = inQuestion.split('],')
+            
+            x = 0
+            
+            while x < len(eachPixEx):
+                if eachPixEx[x] == eachPixInQ[x]:
+                    matchedAr.append(int(currentNum))
+                
+                x+=1
+            
+        except Exception as e:
+            print(str(e))
+
+    x = Counter(matchedAr)
+    print(x)
+    graphX = []
+    graphY = []
+    
+    ylimi = 0
+    
+    for eachThing in x:
+        graphX.append(eachThing)
+        graphY.append(x[eachThing])
+        ylimi = x[eachThing]
+
+
+
+    fig = plt.figure()
+    ax1 = plt.subplot2grid((4,4),(0,0), rowspan=1, colspan=4)
+    ax2 = plt.subplot2grid((4,4),(1,0), rowspan=3,colspan=4)
+
+    ax1.imshow(iar)
+    ax2.bar(graphX,graphY,align='center')
+    plt.ylim(400)
+
+    xloc = plt.MaxNLocator(12)
+    ax2.xaxis.set_major_locator(xloc)
+
+    plt.show()
+
+whatNumIsThis('images/test/test1.png')
---
+++ train.py
@@ -0,0 +1,21 @@
+from PIL import Image
+import numpy as np
+
+#The runs all the images from my training data and add the pixels details
+#to the numArExx.txt filr
+def createExamples():
+    numberArrayExamples = open('numArEx.txt','a')
+    numbersWeHave = range(1,10)
+    for eachNum in numbersWeHave:
+        for furtherNum in numbersWeHave:
+
+            imgFilePath = 'images/numbers/'+str(eachNum)+'.'+str(furtherNum)+'.png'
+            ei = Image.open(imgFilePath)
+            eiar = np.array(ei)
+            eiarl = str(eiar.tolist())
+
+            lineToWrite = str(eachNum)+'::'+eiarl+'\n'
+            numberArrayExamples.write(lineToWrite)
+
+
+createExamples()
